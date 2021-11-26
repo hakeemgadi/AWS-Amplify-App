b6976349d9cbca456aeac3699340659f22328205
hakeemgadi <hakeem.gadi@gmail.com>
2021-11-26 23:09:16 +0400

    added auth

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-react'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator loginMechanisms={['email']}>
+      {({ signOut, user }) => (
+        <main>
+          <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>We now have Auth!</h1>
+          </header>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
